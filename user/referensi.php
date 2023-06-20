5254f5bb5648fda6998480e92517bea887f7904f
Yufri Luttu <yufridonluttu@gmail.com>
2023-06-21 00:15:26 +0800

    dashboar admin dan user

---
+++ user/referensi.php
@@ -0,0 +1,68 @@
+<?php 
+session_start();
+unset($_SESSION['menu']);
+$_SESSION['menu'] = 'referensi';
+?>
+<?php require_once './header.php';?>
+<div class="row">
+    <!-- Area Chart -->
+    <!-- Button trigger modal -->
+    <div class="col-lg-12">
+        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">
+            + Tambah data
+        </button>
+        <div class="card">
+            <!-- <div class="card-header">
+                Featured
+            </div> -->
+            <div class="card-body">
+                <!-- DataTales Example -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Daftar Lemari</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Lemari</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>System Architect</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once './footer.php';?>
